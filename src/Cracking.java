4a02d326cfda061180cce00a279c7ec72f0c91d5
ritwika-reddy <rkanchar@purdue.edu>
2018-01-28 17:26:42 -0500

    Practicing

--- src/Cracking.java
+++ src/Cracking.java
@@ -1,20 +1,9 @@
-import trees.Node;
-import trees.Problem1;
+import arrays_and_strings.*;
 
 public class Cracking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Cracking the Coding Interview"); 
-		Node root = new Node(1); 
-		root.left = new Node(2);
-		root.left.right = new Node(3); 
-		System.out.println(root);
-
-		Problem1 pr1 = new Problem1(); 
-		
-		System.out.println(pr1.CheckBalanced(root));
-		
-		
+	
+	
 	}
 }
